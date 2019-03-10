885edfa5ac6e90f746c0bbfbfe7426353c04ba99
GSoster <guilhermesosters@gmail.com>
2019-03-10 14:48:40 -0300

    Updated program to allow easy insertion of tasks

--- EntityFrameworkTablePerType/EntityFrameworkTablePerType/Program.cs
+++ EntityFrameworkTablePerType/EntityFrameworkTablePerType/Program.cs
@@ -12,34 +12,49 @@ namespace EntityFrameworkTablePerType
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.            
+         
+            TasksContext context = new TasksContext();
 
+            var task = context.Tasks.FirstOrDefault(t => t.Name == "ProjecTask");
+            if (task!=null)
+            {
+                Console.WriteLine("task exist");
+            }
+           
+            Console.WriteLine("Task is project? ", task is ProjectTask);
+
+            Console.ReadKey();
+            
+        }
+
+
+        static void InsertTrelloTask(string name, TasksContext context)
+        {
             TrelloTask trelloTask = new TrelloTask()
             {
-                Name = "Trello Task 1 ",
+                Id = Guid.NewGuid(),
+                Name = name,
                 Description = "Description da trello task 1",
                 IsTrelloTask = true,
                 BoardName = "Board teste",
                 BoardId = "xyz"
             };
-
-            TasksContext context = new TasksContext();
             context.Tasks.Add(trelloTask);
             context.SaveChanges();
-            Console.WriteLine("Trello Task Inserted!");
-            var t = context.Tasks.FirstOrDefault();
-            if (t != null)
-            {
-                Console.WriteLine("Task != null, " + t.Name);
-            }
-            else
-            {
-                Console.WriteLine("Task == null");
-            }
-            Console.ReadKey();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        static void InsertProjectTask(string name, TasksContext context)
+        {
+            ProjectTask projectTask = new ProjectTask()
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                IsProjectTask = true,
+                ProjectName = "My Project",
+                PWAInstance = "ILABS"
+            };
+            context.Tasks.Add(projectTask);
+            context.SaveChanges();
         }
     }
 }
